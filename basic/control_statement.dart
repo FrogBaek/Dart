23dbe5ab5bf95836f1e19143e199b28d1d7797d4
FrogBaek <rnsdls1999@naver.com>
2023-04-01 14:21:47 +0900

    기본기 최종본

---
+++ basic/control_statement.dart
@@ -0,0 +1,216 @@
+void main() {
+// if 문
+  int number = 3;
+
+  if (number % 3 == 0) {
+    print('나머지가 0입니다.');
+  } else if (number % 3 == 1) {
+    print('나머지가 1입니다.');
+  } else {
+    print('나머지가 2입니다.');
+  }
+
+
+  // switch문
+  int number = 3;
+
+  switch(number % 3){
+    case 0:
+      print('나머지가 0입니다.');
+      break;
+
+    case 1:
+      print('나머지가 1입니다.');
+      break;
+
+    default:
+      print('나머지가 2입니다.');
+      break;
+  }
+
+
+  // for loop
+  for(int i = 0; i < 10; i++){
+    print(i);
+  }
+
+  int total = 0;
+
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  for(int i = 0; i < numbers.length; i++){
+    total += numbers[i];
+  }
+
+  print(total);
+
+  total = 0;
+
+  for (int number in numbers) {
+    total += number;
+  }
+
+  print(total);
+
+  for(int i = 0; i < 10; i++){
+    total += 1;
+
+    if (total == 5){
+      break;
+    }
+  }
+
+  for (int i = 0; i < 10; i++){
+    if (i == 5){
+      continue;
+    }
+    print(i);
+  }
+
+  print(total);
+
+
+  // while loop
+  int total = 0;
+
+  while(total < 10){
+    total += 1;
+  }
+
+  print(total);
+
+  total = 0;
+
+  do {
+    total += 1;
+  } while (total < 10);
+  // do(먼저 한 번 하고) while(~까지 실행)
+  // do while문은 거의 안 씀
+
+  print(total);
+
+  while(total < 10){
+    total += 1;
+
+    if (total == 5){
+      break;
+    }
+  }
+}
+
+
+// enum
+enum Status{
+  approved,
+  pending,
+  rejected
+}
+
+void main2() {
+  Status status = Status.pending;
+  // 그냥 String같은 타입으로 처리하면 되지 않으냐?
+  // 기능은 똑같음 하지만 뉘앙스(사람의 입장에서) 차이가 생기므로
+  // enum으로 관리하기 편한 경우 쓰게 될 거임
+
+  if (status == Status.approved){
+    print('승인입니다.');
+  } else if (status == Status.pending){
+    print('대기입니다.');
+  } else {
+    print('거절입니다.');
+  }
+
+
+  // function
+  addNumbers(10, 30);
+
+  addNumbers2(x: 10, z: 30, y: 20);
+  addNumbers2(x: 10, y: 20);
+}
+
+
+// 세 개의 숫자 (x, y, z)를 더하고 짝수인지 홀수인지 알려주는 함수
+// parameter / argument - 매개변수 / 인수(인자)
+// positional parameter - 순서가 중요한 파라미터
+//  ex. addNumbers(10, 20, 30);
+// optional parameter - 있어도 되고 없어도 되는 파라미터
+
+
+addNumbers(int x, [int y = 20, int? z]) {
+  // optional parameter를 줄 때, int? 식으로 null값을 허용하면
+  // sum 변수에서 null과 int를 더해야하는 에러가 발생하기 때문에
+  // 기본값을 지정해주는 것이 좋음!
+  int sum = x + y + z;
+  // y는 정상, z는 에러 발생
+
+  print('x : $x');
+  print('y : $y');
+  print('z : $z');
+
+  if (sum % 2 == 0) {
+    print('짝수입니다.');
+  } else {
+    print('홀수입니다.');
+  }
+}
+
+// named parameter - 이름이 있는 파라미터 (순서가 중요하지 않다.)
+addNumbers2(int x, {
+  required int y,
+  int z = 100
+  // 이런 식으로 positional parmeter, optional parameter, named parameter 로 만들어 줄 수 있음!
+}) {
+  int sum = x + y + z;
+}
+
+// 함수의 return 타입을 void, int, double, String 등등으로 지정할 수 있음
+
+
+// arrow function
+// addNumbers3와 같은 함수임!
+int addNumbers3(int x, {
+  required int y,
+  int z = 100
+}) => x + y + z;
+
+
+void main3 () {
+  Operation operation = add;
+
+  int result = operation(10, 20, 30);
+
+  print(result);
+
+  operation = substract;
+
+  int result2 = operation(10, 20, 30);
+
+  print(result2);
+
+  int result3 = calculate(30, 40, 50, add);
+
+  print(result3);
+
+  int result4 = calculate(40, 50, 60, substract);
+
+  print(result4);
+}
+
+
+
+// typedef
+// signature
+typedef Operation = int Function(
+  int x, int y, int z
+);
+
+// 아래의 함수들은 typedef signature의 return타입과 parameter가 모두 일치해야함!
+
+// 더하기
+int add(int x, int y, int z) => x + y + z;
+
+// 빼기
+int substract(int x, int y, int z) => x - y -z;
+
+// 실제로 쓰이는 예
+int calculate(int x, int y, int z, Operation operation) => operation(x, y, z);
--- basic/date_type.dart
+++ basic/date_type.dart
@@ -67,6 +67,42 @@ void main () {
   name2 = 5;
   //var은 선언된 변수의 타입으로 정해지는데, 한 번 정해지면 수정 불가!
 
+  var z = 1;
+  z = '이미 선언과 동시에 int로 초기화 했기에 타입 변경 불가';
+  
+  dynamic c = 3;
+  // 선언과 동시에 int로 초기화 했음에도 dynamic이기에 타입 변경 가능
+  c = 3.0;
+  
+  
+  // 하지만 선언 후, 초기화를 하는 경우에는 var이나 dynamic이나 같음!
+  var x;
+  
+  print(x);
+  
+  x = 1.0;
+  print(x);
+  
+  x = 'Hi';
+  print(x);
+  
+  x = false;
+  print(x);
+  
+  
+  dynamic y;
+  
+  print(y);
+  
+  y = 1.0;
+  print(y);
+  
+  y = 'Hi';
+  print(y);
+  
+  y = true;
+  print(y);
+
 
   // null
 
@@ -84,6 +120,12 @@ void main () {
   print(name2);
   print(name2!);
 
+  double? number = 4.0;
+
+  number = null;
+
+  number ??= 3.0;
+  
   
   // final vs const
   final name = '전투개구리';
@@ -99,5 +141,151 @@ void main () {
   print(now);
 
   const DateTime now2 = DateTime.now();
-}
 
+
+
+
+
+  // Operator
+  int numberA = 1;
+  int numberB = 2;
+
+  print(numberA > numberB);
+  print(numberA < numberB);
+  print(numberA >= numberB);
+  print(numberA <= numberB);
+  print(numberA == numberB);
+  print(numberA != numberB);
+
+
+  //Type 비교
+  int numberC = 1;
+
+  print(numberC is int);
+  print(numberC is String);
+
+  print(numberC is! int);
+  print(numberC is! String);
+
+
+  // 비교 연산자
+  bool result = 12 > 18 && 1 > 0;
+
+  print(result);
+
+  bool result2 = 12 > 18 && 0 > 1;
+
+  print(result2);
+
+  bool result3 = 12 > 18 || 1 > 0;
+
+  print(result3);
+
+  bool result4 = 12 < 18 || 1 < 0;
+
+  print(result4);
+
+  bool result5 = 12 < 18 && 1 > 0;
+
+  print(result5);
+
+
+  // List
+  List<String> aespa = ['카리나', '윈터'];
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  print(aespa);
+  print(numbers);
+
+  // index
+  print(aespa[0]);
+  print(aespa[1]);
+  print(aespa[2]);
+  // Index out of range: 존재하지 않으므로 오류 발생!
+
+  print(aespa.length);
+
+  aespa.add('닝닝');
+
+  print(aespa);
+
+  aespa.remove('닝닝');
+
+  print(aespa);
+
+  print(aespa.indexOf('윈터'));
+  // 몇 번째 인덱스에 존재하는지!
+
+  
+  // Map (Key - value)
+  Map<String, String> dictionary = {
+    'Combat Frog': '전투개구리',
+    'Frozen Frog': '얼어붙은 개구리',
+    'Frog': '개구리'
+  };
+
+  print(dictionary);
+
+  Map<String, bool> isCombarFrog = {
+    'Combat Frog': true,
+    'Frozen Frog': false,
+    'Frog': false
+  }
+
+  print(isCombarFrog);
+
+  isCombarFrog.addAll({
+    'Horse': false,
+    'Zebra': false,
+  });
+
+  print(isCombarFrog);
+
+  print(isCombarFrog['Horse']);
+  // 항상 value가 아닌 key로 인덱싱!!
+
+  isCombarFrog['Elephant'] = false;
+  // 이렇게 추가도 가능
+
+  print(isCombarFrog);
+
+  isCombarFrog['Hores'] = true;
+  // 이렇게 수정도 가능
+
+  isCombarFrog.remove('Frog');
+  // 이렇게 삭제도 가능
+
+  print(isCombarFrog);
+
+  print(isCombarFrog.keys);
+  // key들만 가져오기
+
+  print(isCombarFrog.values);
+  // value들만 가져오기
+
+  
+  // Set
+  // List와 거의 동일한데, List와는 다르게 중복 값을 받지 않음
+  // 즉, 자동으로 중복 처리해줌!
+  final Set<String> names = {
+    'Combat Frog',
+    'Flutter',
+    'Aespa',
+    'Flutter'
+    // 같은 값이 들어갔으므로 오류 발생!
+  };
+
+  print(names);
+
+  names.add('Winter');
+
+  print(names);
+
+  names.remove('Winter');
+
+  print(names);
+
+  print(names.contains('Flutter'));
+
+
+}
